1d9554c4fea3257a444471c0b53a539686a88ae2
Daniel Victoriano <djose1164@gmail.com>
2022-02-24 18:30:35 +0000

    Need to complete.

---
+++ cpp-primer/exer3-20.cpp
@@ -0,0 +1,27 @@
+/*
+Exercise 3.20: Read a set of integers into a vector. Print the sum of each pair of
+adjacent elements. Change your program so that it prints the sum of the first and
+last elements, followed by the sum of the second and second-to-last, and so on.
+*/
+
+#include <iostream>
+#include <vector>
+
+int main(void)
+{
+    std::vector<int> vec;
+    int num{}, len{};
+    while (std::cin >> num && ++len)
+        vec.push_back(num);
+    int sum{};
+    for (auto cur : vec)
+        sum += cur;
+    std::cout << "The sum of each pair is: " << sum << std::endl;
+    sum = 0;
+    auto len_c = len;
+    for (size_t i{}; i < len; ++i)
+        sum += vec[i] + vec[--len_c];
+    std::cout << "Second: " << sum << std::endl;
+    
+    return 0;
+}
