ff6abc9405086bfd3efe9110015ee3a73af97cde
Daniel Victoriano <djose1164@gmail.com>
2022-06-23 19:40:25 -0400

    Damn powercut; fix exercise.

--- cpp-primer/exer3-44.cpp
+++ cpp-primer/exer3-44.cpp
@@ -6,31 +6,32 @@ type alias for the type of the loop control variables.
 
 int main(void)
 {
-	int ia[3][4] = {{0, 1, 2, 3},
+	using int_array = int[4];
+	int_array ia[3] = {{0, 1, 2, 3},
                     {4, 5, 6, 7},
                     {8, 9, 10, 11}};
 	
 	// With for-range.
-	for (auto (&p)[4]: ia)
+	for (int_array &p: ia)
 	{
-		for (auto& q: p)
+		for (int& q: p)
 			std::cout << q << " ";
 	}
 	std::cout << "\n=======" << std::endl;
 
 	// Using subscripts.
-	for (auto i = 0; i < 3; ++i)
+	for (size_t i = 0; i < 3; ++i)
 	{
-		for (auto j = 0; j < 4; ++j) 	
-			std::cout << ia[i][j];
+		for (size_t j = 0; j < 4; ++j) 	
+			std::cout << ia[i][j] << ' ';
 		std::cout << std::endl;
 	}
 	std::cout << "=======" << std::endl;
 	
 	// Using pointers.
-	for (auto (*p)[4] = ia; p != ia + 3; ++p)
+	for (int_array *p = ia; p != ia + 3; ++p)
 	{
-		for (auto *q = *p; q != *p + 4; ++q)
+		for (int *q = *p; q != *p + 4; ++q)
 		std::cout << *q << ' ';
 	std::cout << std::endl;
 	}
